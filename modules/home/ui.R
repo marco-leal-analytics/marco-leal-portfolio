f20cc1e07b840344e551d6bc6f2cc5388b51a2ef
Marco Aurelio Valles Leal <marco.leal.analytics@gmail.com>
2026-09-03 10:31:49 -0300

    ajusta pagian perfil

--- modules/home/ui.R
+++ modules/home/ui.R
@@ -2,14 +2,17 @@ library(shiny)
 
 # Aba inicial: incorpora a apresentação Quarto pré-renderizada em largura total.
 mod_home_ui <- function(id) {
-  ns <- NS(id)
+  document_path <- file.path("modules", "home", "home-marco-leal-analytics.html")
+  document_mtime <- file.info(document_path)$mtime
+  document_version <- if (is.na(document_mtime)) "latest" else format(document_mtime, "%Y%m%d%H%M%S")
+
   tagList(
     tags$div(class = "qmd-home-wrapper",
       tags$section(class = "resume-main qmd-document-shell",
         tags$iframe(
           class = "qmd-document-frame",
-          src = "home-document/home-marco-leal-analytics.html",
-          title = "Sobre Marco Leal Analytics",
+          src = paste0("home-document/home-marco-leal-analytics.html?v=", document_version),
+          title = "Perfil de Marco Leal Analytics",
           scrolling = "no",
           onload = "this.style.height = this.contentWindow.document.documentElement.scrollHeight + 'px';"
         )
